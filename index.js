91c6f06d95acd5bc13c8c4121bed1571cf0f0701
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-09-27 22:44:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const bars = document.querySelector(".fa-bars");
 const sidebar = document.querySelector(".sidebar");
 
-
+bars.addEventListener("click", ()=>{
+  sidebar.classList.toggle('show-sidebar');
+})
