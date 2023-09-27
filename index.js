cbf123b85e63401e0fc0eea89b2532d03441a967
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-09-27 22:40:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bars = document.querySelector(".fa-bars");
 const sidebar = document.querySelector(".sidebar");
+const closingButton = document.querySelector(".fa-times");
 
 bars.addEventListener("click", ()=>{
   sidebar.classlist.toggle('show-sidebar');
